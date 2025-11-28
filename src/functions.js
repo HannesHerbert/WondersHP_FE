80eea409b00cb9e2e21409cf5b0f7390b327a566
HannesHerbert <hannespetri89@gmail.com>
2025-11-28 15:56:49 +0100

    Updates

--- src/functions.js
+++
@@ -1,9 +0,0 @@
-export function getDateString(date) {
-    const dateArr = date.split('-');
-    let dateStr = '';
-    for(let i = dateArr.length-1; i >= 0; i--) {
-        dateStr += dateArr[i] + (i !== 0 ? '.' : '')
-    }
-    
-    return dateStr;
-}
